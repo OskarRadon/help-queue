a479aadf6fc98a1653cd75b6e34cec8861ecdd45
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-04 10:18:49 -0700

    Add ability to save ticket to server from user route

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('ticket');
+  },
+  actions: {
+    saveTicket(params) {
+      var newTicket = this.store.createRecord('ticket', params);
+      newTicket.save();
+      this.transitionTo('index');
+    }
+  }
 });
