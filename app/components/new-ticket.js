4cab5ed567352f9a2858d94bc6a212371569ea16
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-04 11:43:31 -0700

    Add timestamp generated when each ticket is made

--- app/components/new-ticket.js
+++ app/components/new-ticket.js
@@ -7,10 +7,17 @@ export default Ember.Component.extend({
       this.set('showTicketForm', true);
     },
     saveTicket() {
+      var m = new Date();  //used for timestamp
       var params = {
         user: this.get('user'),
         title: this.get('title'),
-        text: this.get('text')
+        text: this.get('text'),
+        timestamp:(m.getUTCHours() - 7) + ":" +
+                   m.getUTCMinutes() + ":" +
+                   m.getUTCSeconds() + " " +
+                   m.getUTCFullYear() +"/"+
+                  (m.getUTCMonth()+1) +"/"+
+                   m.getUTCDate()
       };
       this.set('showTicketForm', false);
       this.sendAction('saveTicket', params);
--- app/models/ticket.js
+++ app/models/ticket.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   user: DS.attr(),
   title: DS.attr(),
-  text: DS.attr()
+  text: DS.attr(),
+  timestamp: DS.attr()
 });
